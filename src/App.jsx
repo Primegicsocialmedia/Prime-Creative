08c0dd6efd5fa79cd0cb466b56aa2912df80bd73
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-03-25 19:51:12 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-4xl mt-4 pt-4 font-bold flex items-center underline justify-center">
+        Prime Creative
+      </div>
+    </>
+  );
+}
+
+export default App;
