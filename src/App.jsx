35eae511df74fd74f60882c81ef9d234ff17808b
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-11 18:08:34 +0200

    hero left completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-
-
+import Hero from "./sections/Hero";
 
 function App() {
-
-
   return (
-  <div className="text-3xl bold flex justify-center items-center h-screen bg-slate-900 text-white">
- <h2>Prime Creative</h2>
-  </div>
+    <>
+      <Hero />
+    </>
   );
 }
 
